7c842a7ea402631e69a873392a0463b9b0084812
Moliendo Code <molyrrk@gmail.com>
2022-01-14 22:14:56 -0300

    Agregado Test y fn search

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,14 @@ use std::error::Error;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("El contenido del archivo es: \n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
 }
+
+
 
 pub struct Config {
     pub query: String,
@@ -23,5 +27,34 @@ impl Config {
         let filename = args[2].clone();
     
         Ok(Config { query, filename })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
     }
 }
